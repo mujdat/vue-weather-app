c60b5ab3c5b99ab7732c05695a7f477ab13981b6
Müjdat Korkmaz <hi@mujdat.dev>
2020-09-12 10:32:53 +0200

    bumped serialize js to 3.1.0

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,9 @@
         <logo>☔️</logo>
       </div>
       <div>
-        <input 
-          type="text" 
-          class="search-bar" 
+        <input
+          type="text"
+          class="search-bar"
           placeholder="How's the weather like in..."
           v-model="query"
           @keypress="fetchWeather"
@@ -16,20 +16,26 @@
 
       <div class="weather-wrapper" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
-          <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
+          <div class="location">
+            {{ weather.name }}, {{ weather.sys.country }}
+          </div>
           <div class="date">{{ dateBuilder() }}</div>
         </div>
 
         <div class="weather-box">
-          <div class="temp"> {{ Math.round(weather.main.temp) }}°C</div>
+          <div class="temp">{{ Math.round(weather.main.temp) }}°C</div>
           <div class="desc">{{ weather.weather[0].description }}</div>
         </div>
       </div>
     </main>
     <footer class="animate__animated animate__fadeIn slower">
-      made by<a href="https://github.com/mujdat"><i class="fa fa-github"></i> mujdat</a> /
-      <a href="https://mujdat.dev"><i class="fa fa-globe"></i> homepage</a> /
-      <a href="https://twitter.com/mujdatkorkmaz"><i class="fa fa-twitter"></i> twitter</a>        
+      made by<a href="https://github.com/mujdat"
+        ><i class="fa fa-github"></i> mujdat</a
+      >
+      / <a href="https://mujdat.dev"><i class="fa fa-globe"></i> homepage</a> /
+      <a href="https://twitter.com/mujdatkorkmaz"
+        ><i class="fa fa-twitter"></i> twitter</a
+      >
     </footer>
   </div>
 </template>
@@ -38,78 +44,84 @@
 export default {
   name: 'App',
   metaInfo() {
-        return { 
-            title: "Vue.js Weather App",
-            titleTemplate: '%s - Client-Side Vue.js Weather App with OpenWeatherMap API.',
-            meta: [
-                { name: 'description', content:  'Client-Side Vue.js Weather App with OpenWeatherMap API.'},
-                { property: 'og:title', content: "Vue.js Weather App"},
-                { property: 'og:site_name', content: 'Vue.js Weather App'},
-                {property: 'og:type', content: 'website'},    
-                {name: 'robots', content: 'index,follow'} 
-            ]
-        }
-    },
-  data () {
-    return  {
+    return {
+      title: 'Vue.js Weather App',
+      titleTemplate:
+        '%s - Client-Side Vue.js Weather App with OpenWeatherMap API.',
+      meta: [
+        {
+          name: 'description',
+          content: 'Client-Side Vue.js Weather App with OpenWeatherMap API.',
+        },
+        { property: 'og:title', content: 'Vue.js Weather App' },
+        { property: 'og:site_name', content: 'Vue.js Weather App' },
+        { property: 'og:type', content: 'website' },
+        { name: 'robots', content: 'index,follow' },
+      ],
+    };
+  },
+  data() {
+    return {
       api_key: process.env.VUE_APP_API_KEY,
-      base_url: process.env.VUE_APP_BASE_URL, 
+      base_url: process.env.VUE_APP_BASE_URL,
       query: '',
-      weather: {}
-    }
+      weather: {},
+    };
   },
   methods: {
-    fetchWeather (e) {
+    fetchWeather(e) {
       if (e.key == 'Enter') {
-        fetch(`${this.base_url}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
-          .then(res => {
+        fetch(
+          `${this.base_url}weather?q=${this.query}&units=metric&APPID=${this.api_key}`
+        )
+          .then((res) => {
             return res.json();
-          }).then(this.setResults);
+          })
+          .then(this.setResults);
       }
     },
-    setResults (results) {
+    setResults(results) {
       this.weather = results;
     },
 
-    dateBuilder () {
+    dateBuilder() {
       let d = new Date();
       let months = [
-          "January", 
-          "February", 
-          "March", 
-          "April", 
-          "May", 
-          "June", 
-          "July", 
-          "August", 
-          "September", 
-          "October", 
-          "November", 
-          "December"
-                   ];
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December',
+      ];
       let days = [
-          "Sunday", 
-          "Monday", 
-          "Tuesday", 
-          "Wednesday", 
-          "Thursday", 
-          "Friday", 
-          "Saturday"
-                 ];
-      
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday',
+      ];
+
       let day = days[d.getDay()];
       let date = d.getDate();
       let month = months[d.getMonth()];
       let year = d.getFullYear();
-      
+
       return `${day}, ${date}. ${month} ${year}`;
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style>
-
 * {
   margin: 0;
   padding: 0;
@@ -118,8 +130,8 @@ export default {
 
 body {
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-   Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-   color: #333;
+    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  color: #333;
 }
 
 #app {
@@ -149,7 +161,7 @@ logo {
   width: 100%;
 }
 
-.search-box .search-bar{
+.search-box .search-bar {
   font-size: 24px;
   display: block;
   width: 100%;
@@ -161,10 +173,10 @@ logo {
   background-color: rgba(0, 0, 0, 0.3);
   border-radius: 7px;
   transition: 0.5s;
-  color: #EEF0F2;
+  color: #eef0f2;
 }
 
-.search-box .search-bar:focus{
+.search-box .search-bar:focus {
   background-color: rgba(0, 0, 0, 0.5);
 }
 
@@ -179,7 +191,7 @@ logo {
   padding: 50px;
   text-align: center;
   margin-top: 40px;
-  font-weight: 100;   
+  font-weight: 100;
   color: white;
   width: 100%;
 }
@@ -195,7 +207,7 @@ logo {
 .date {
   font-size: 20px;
   letter-spacing: 1px;
-    margin-bottom: 50px;
+  margin-bottom: 50px;
 }
 
 .temp {
@@ -208,9 +220,9 @@ logo {
   font-style: italic;
 }
 
-footer { 
-  /* 
-    TODO: Use CSS flexbox or some lightweight CSS framework for better mobile compatibility. 
+footer {
+  /*
+    TODO: Use CSS flexbox or some lightweight CSS framework for better mobile compatibility.
   */
   position: fixed;
   left: 0;
@@ -220,15 +232,15 @@ footer {
   color: white;
   text-align: center;
   font-weight: 200;
-  color: #EEF0F2;
+  color: #eef0f2;
   height: 60px;
   line-height: 60px;
   background-color: rgba(0, 0, 0, 0.5);
   letter-spacing: 3px;
 }
 
-footer a{
-  color: #EEF0F2;
+footer a {
+  color: #eef0f2;
   margin: 0 10px 0 10px;
   padding-bottom: 5px;
   text-decoration: none;
@@ -236,22 +248,22 @@ footer a{
 }
 
 footer a:hover {
-  border-bottom: 1px solid #EEF0F2;
+  border-bottom: 1px solid #eef0f2;
   padding-bottom: 5px;
 }
-@media all and (max-width:414px) {
+@media all and (max-width: 414px) {
   logo {
     font-size: 75px;
   }
-  .location, .temp {
+  .location,
+  .temp {
     font-size: 32px;
   }
 }
 
-@media all and (max-width:320px) {
+@media all and (max-width: 320px) {
   .weather-wrapper {
     padding: 25px;
   }
 }
-
 </style>
